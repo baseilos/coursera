293f08a87f48618c0e4dae61289cbe8f79be0ffe
Jozef Lang <developer@jozeflang.com>
2016-12-03 21:37:52 +0100

    First verrsion of TxHandler

--- bitcoin-cryptocurrencies-technologies/ScroogeCoin/TxHandler.java
+++ bitcoin-cryptocurrencies-technologies/ScroogeCoin/TxHandler.java
@@ -1,4 +1,9 @@
+import java.util.*;
+import java.security.PublicKey;
+
 public class TxHandler {
+
+    private final UTXOPool utxoPool;
 
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
@@ -6,7 +11,7 @@ public class TxHandler {
      * constructor.
      */
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        this.utxoPool = new UTXOPool(utxoPool);
     }
 
     /**
@@ -19,7 +24,36 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // IMPLEMENT THIS
+        // Checking condition partially 5
+        double sumOfOutputs = 0; 
+        for (int index = 0; index < tx.numOutputs(); index++) {
+            Transaction.Output txOutput = tx.getOutput(index);
+            if (txOutput.value < 0) {
+                return false;
+            } else {
+                sumOfOutputs += txOutput.value;
+            }
+        }
+
+        // Checking condition 1,2,3,4 & partially 5
+        double sumOfInputs = 0;
+        Map<PublicKey, Double> seenSpentOutputs = new HashMap<>();
+        for (int index = 0; index < tx.numInputs(); index++) {
+            Transaction.Input txInput = tx.getInput(index);
+            UTXO assocUtxo = new UTXO(txInput.prevTxHash, txInput.outputIndex); 
+            Transaction.Output assocOutput = utxoPool.getTxOutput(assocUtxo);
+            if (assocOutput != null 
+                && Crypto.verifySignature(assocOutput.address, tx.getRawDataToSign(index), txInput.signature)
+                && (seenSpentOutputs.get(assocOutput.address) == null || seenSpentOutputs.get(assocOutput.address) != assocOutput.value)) {
+                sumOfInputs += assocOutput.value;
+                seenSpentOutputs.put(assocOutput.address, assocOutput.value);
+            } else {
+                return false; // Input not valid hence transaction is not valid
+            }
+        }
+        
+        // Checking condition 5
+        return sumOfInputs >= sumOfOutputs;
     }
 
     /**
@@ -28,7 +62,24 @@ public class TxHandler {
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        // IMPLEMENT THIS
+        List<Transaction> validTxs = new ArrayList<>();
+        for (Transaction processedTx : possibleTxs) {
+            if (isValidTx(processedTx)) {
+                // Add to validTxs list
+                validTxs.add(processedTx);
+                // Update utxoPool
+                for (int index = 0; index < processedTx.numInputs(); index++) {
+                    Transaction.Input txInput = processedTx.getInput(index);
+                    UTXO assocUtxo = new UTXO(txInput.prevTxHash, txInput.outputIndex);
+                    utxoPool.removeUTXO(assocUtxo);
+                }
+                for (int index = 0; index < processedTx.numOutputs(); index++) {
+                    utxoPool.addUTXO(new UTXO(processedTx.getHash(), index), processedTx.getOutput(index));
+                }
+            }
+        }
+
+        return validTxs.toArray(new Transaction[] {});
     }
 
 }
